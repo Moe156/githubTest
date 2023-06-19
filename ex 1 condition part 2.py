15d5cbce6d86f459f2719b06e77b937a0f472ba3
Moe156 <136538315+Moe156@users.noreply.github.com>
2023-06-20 01:07:25 +0200

    Add files via upload

---
+++ ex 1 condition part 2.py
@@ -0,0 +1,8 @@
+num1=int(input("enter num1 "))
+num2=int(input("enter num2 "))
+p=num1*num2
+s=num1+num2
+if int(p)>=1000:
+    print("the product is: "+str(p))
+else:
+    print("the sum is: "+str(s))
